b3e24ab64a8fd3d2cb55451eb93283e0105e97bb
Vijay R K <vkalangu@in.ibm.com>
2017-06-29 13:56:35 +0530

    initial commit

---
+++ singleinstance.tf
@@ -0,0 +1,19 @@
+provider "aws" {
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
+  region     = "${var.region}"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-13be557e"
+  instance_type = "t2.micro"
+
+  provisioner "local-exec" {
+    command = "echo ${aws_instance.example.public_ip} > ip_address.txt"
+  }
+}
+
+resource "aws_eip" "ip" {
+  instance = "${aws_instance.example.id}"
+}
+
